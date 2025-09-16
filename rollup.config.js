800872a41579505235babe4d6e1b192f4ec5e4ae
darwin <mecci1999@163.com>
2025-09-16 10:49:22 +0800

    build: 更新构建配置并添加tslib依赖
    - 添加tslib依赖以支持TypeScript helper函数
    - 扩展tsconfig.build.json配置，增加严格模式等编译选项
    - 更新rollup配置，启用terser压缩并优化external依赖处理

--- rollup.config.js
+++ rollup.config.js
@@ -4,11 +4,21 @@ const dts = require('rollup-plugin-dts');
 const tscAlias = require('rollup-plugin-tsc-alias');
 const json = require('@rollup/plugin-json');
 const alias = require('@rollup/plugin-alias');
-// const { terser } = require('rollup-plugin-terser');
+const { terser } = require('rollup-plugin-terser');
+const pkg = require('./package.json');
+
+// 获取所有依赖作为external，但排除tslib（需要内联helper函数）
+const external = [
+  ...Object.keys(pkg.dependencies || {}).filter(dep => dep !== 'tslib'),
+  ...Object.keys(pkg.peerDependencies || {}),
+  // Node.js内置模块
+  'fs', 'path', 'os', 'crypto', 'util', 'events', 'stream', 'http', 'https', 'net', 'dgram', 'perf_hooks', 'zlib', 'v8'
+];
 
 module.exports = [
   {
     input: './src/index.ts',
+    external,
     output: [
       {
         dir: './dist',
@@ -34,11 +44,19 @@ module.exports = [
         tsconfig: './tsconfig.build.json'
       }),
       tscAlias(),
-      // terser({
-      //   format: {
-      //     comments: true
-      //   }
-      // }),
+      terser({
+        format: {
+          comments: false
+        },
+        compress: {
+          drop_console: false,
+          drop_debugger: true,
+          pure_funcs: ['console.log']
+        },
+        mangle: {
+          reserved: ['Star', 'Service', 'Context']
+        }
+      }),
       filesize(),
       json()
     ]
